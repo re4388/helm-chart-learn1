88b2bf714cb7f1ab5b4e577acd8269ad0189c935
ben.hu <ben.hu@wemoscooter.com>
2025-07-19 15:39:45 +0800

    try fix swagger

--- application/src/main/java/com/example/demoapp/infrastructure/config/OpenApiConfig.java
+++ application/src/main/java/com/example/demoapp/infrastructure/config/OpenApiConfig.java
@@ -4,9 +4,11 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class OpenApiConfig {
+public class OpenApiConfig implements WebMvcConfigurer { // Implement WebMvcConfigurer
 
     @Bean
     public OpenAPI customOpenAPI() {
@@ -15,5 +17,11 @@ public class OpenApiConfig {
                         .title("Demo App API")
                         .version("1.0")
                         .description("API documentation for the Demo Spring Boot Application"));
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/webjars/**")
+                .addResourceLocations("classpath:/META-INF/resources/webjars/");
     }
 }
